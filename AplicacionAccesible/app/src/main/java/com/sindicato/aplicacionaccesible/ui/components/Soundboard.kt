c8fe063ceb36faae6cac526b31e3b67742eb81be
lucasquieresopa <101714093+lucasquieresopa@users.noreply.github.com>
2026-05-28 15:04:17 -0300

    Added predefined templates with buttons and UI to add new to the grid

--- AplicacionAccesible/app/src/main/java/com/sindicato/aplicacionaccesible/ui/components/Soundboard.kt
+++ AplicacionAccesible/app/src/main/java/com/sindicato/aplicacionaccesible/ui/components/Soundboard.kt
@@ -1,21 +1,27 @@
 package com.sindicato.aplicacionaccesible.ui.components
 
 import android.annotation.SuppressLint
+import android.util.Log
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedIconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -27,6 +33,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sindicato.aplicacionaccesible.ui.screens.soundgrid.SoundButton
@@ -67,8 +75,12 @@ fun Soundboard(viewModel: SoundboardViewModel){
 
 @Composable
 fun SoundGrid(viewModel: SoundboardViewModel) {
+
+    // Toma el template actual de la lista de templates segun un indice
     val currentTemplate = viewModel.templates.getOrNull(viewModel.currentTemplateIndex)
     val totalCells = 20 // Define a max capacity
+
+    Log.d("SoundGrid", "currentTemplate: ${currentTemplate?.buttons}")
 
     LazyVerticalGrid(
         columns = GridCells.Fixed(viewModel.columnCount),
@@ -79,8 +91,13 @@ fun SoundGrid(viewModel: SoundboardViewModel) {
 
             if (buttonAtPosition != null) {
                 // Occupied Cell
+                SoundButtonItem(
+                    name = buttonAtPosition.name,
+                    onClick = { }
+                )
             } else if (viewModel.isEditMode) {
                 // Empty Cell in Edit Mode
+                EmptyCellPlaceholder(onClick = { /* Open Dialog to Add */ })
             }
         }
     }
@@ -158,4 +175,33 @@ fun SoundboardTopBar(viewModel: SoundboardViewModel) {
             }
         }
     )
+}
+
+
+@Composable
+fun SoundButtonItem(name: String, onClick: () -> Unit) {
+    ElevatedButton(
+        onClick = onClick,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(80.dp),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Text(text = name)
+    }
+}
+
+
+@Composable
+fun EmptyCellPlaceholder(onClick: () -> Unit) {
+    OutlinedIconButton(
+        onClick = onClick,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(80.dp),
+        shape = androidx.compose.foundation.shape.RoundedCornerShape(8.dp),
+        border = BorderStroke(1.dp, Color.Gray)
+    ) {
+        Icon(Icons.Default.Add, contentDescription = "Add button")
+    }
 }
--- AplicacionAccesible/app/src/main/java/com/sindicato/aplicacionaccesible/ui/screens/soundgrid/SoundboardViewModel.kt
+++ AplicacionAccesible/app/src/main/java/com/sindicato/aplicacionaccesible/ui/screens/soundgrid/SoundboardViewModel.kt
@@ -15,6 +15,38 @@ class SoundboardViewModel {
     var isEditMode by mutableStateOf(false) // indica si estamos en modo edición
     var columnCount by mutableIntStateOf(2) // cantidad de columnas
 
+
+    init{
+        var defaultButtons = listOf(
+            SoundButton("Ayuda", 0),
+            SoundButton("Aviso", 1),
+            SoundButton("Aplausos", 2),
+            SoundButton("Advertencia", 3),
+            SoundButton("Beso", 4)
+        )
+        _templates.add(Template("Default", defaultButtons))
+
+        var defaultButtons2 = listOf(
+            SoundButton("A", 0),
+            SoundButton("B", 1),
+            SoundButton("C", 2),
+            SoundButton("D", 3),
+            SoundButton("E", 4),
+            SoundButton("F", 5),
+            SoundButton("G", 6),
+            SoundButton("H", 7),
+            SoundButton("I", 8),
+            SoundButton("J", 9),
+            SoundButton("K", 10),
+            SoundButton("L", 11),
+            SoundButton("M", 12),
+            SoundButton("N", 13),
+            SoundButton("O", 14)
+        )
+
+        _templates.add(Template("Alfabeto", defaultButtons2))
+    }
+
     fun addTemplate(name: String) {
         _templates.add(Template(name = name))
         currentTemplateIndex = _templates.size - 1
