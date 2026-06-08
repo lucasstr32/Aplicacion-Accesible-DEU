ec2de1b098f8ee56f8462110e439496ef8f9c563
lucasquieresopa <101714093+lucasquieresopa@users.noreply.github.com>
2026-06-08 18:44:59 -0300

    deleted annoying line

--- AplicacionAccesible/app/src/main/java/com/sindicato/aplicacionaccesible/viewmodel/SoundboardViewModel.kt
+++ AplicacionAccesible/app/src/main/java/com/sindicato/aplicacionaccesible/viewmodel/SoundboardViewModel.kt
@@ -3,7 +3,6 @@ package com.sindicato.aplicacionaccesible.viewmodel
 import android.content.Context
 import android.media.MediaPlayer
 import android.os.Build
-import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
