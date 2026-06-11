0d9de9c847aa743f1d17ccbc138ce5872def32bc
lucasquieresopa <101714093+lucasquieresopa@users.noreply.github.com>
2026-06-10 21:34:19 -0300

    Database number updated

--- AplicacionAccesible/app/src/main/java/com/sindicato/aplicacionaccesible/data/AppDatabase.kt
+++ AplicacionAccesible/app/src/main/java/com/sindicato/aplicacionaccesible/data/AppDatabase.kt
@@ -17,7 +17,7 @@ import com.sindicato.aplicacionaccesible.data.entity.TemplateEntity
             TemplateEntity::class,
             ButtonEntity::class
         ],
-    version = 4,
+    version = 5,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
